517ca11920ae7926ee576aaa6906b0a0e22bc35e
devxdreus <andricrafter007@gmail.com>
2024-07-04 18:42:07 +0800

    fix db

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const BASE_URL =
     process.env.NODE_ENV !== 'production'
-        ? 'http://localhost:3000'
+        ? 'http://localhost:3000/api'
         : 'http://8.215.11.16/api';
 
 export default axios.create({
