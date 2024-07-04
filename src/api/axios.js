8fafa4fb5ffefed6942ce3256154d617b0c019e3
devxdreus <andricrafter007@gmail.com>
2024-07-04 19:01:03 +0800

    fix: image cors

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL =
+export const BASE_URL =
     process.env.NODE_ENV !== 'production'
         ? 'http://localhost:3000/api'
         : 'http://8.215.11.16/api';
--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import { createContext, useEffect, useState } from 'react';
-import axios from '../api/axios';
+import axios, { BASE_URL } from '../api/axios';
 
 export const ProductContext = createContext();
 
@@ -10,108 +10,139 @@ export const ProductProvider = ({ children }) => {
     const [categories, setCategories] = useState([]);
 
     useEffect(() => {
-      const fetchCategories = async () => {
-        try {
-          const response = await axios.get('/categories');
-          const data = response.data.payload;
-          setCategories(data);
-        } catch (error) {
-          console.error("Error fetching categories:", error);
-        }
-      }
+        const fetchCategories = async () => {
+            try {
+                const response = await axios.get('/categories');
+                const data = response.data.payload;
+                setCategories(data);
+            } catch (error) {
+                console.error('Error fetching categories:', error);
+            }
+        };
 
-      const fetchProducts = async () => {
-        try {
-          const response = await axios.get("/products");
-          const data = response.data.payload;
-          setProducts(data);
-        } catch (error) {
-          console.error("Error fetching products:", error);
-        }
-      };
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get('/products');
+                const data = response.data.payload.map((product) => {
+                    product.image = `${BASE_URL}/${product.image}`;
+                    return product;
+                });
+                setProducts(data);
+            } catch (error) {
+                console.error('Error fetching products:', error);
+            }
+        };
 
-      fetchCategories();
-      fetchProducts();
+        fetchCategories();
+        fetchProducts();
     }, []);
 
     const addProductToCart = (product) => {
-      setCart((prevCart) => {
-        const existingProduct = prevCart.find((item) => item.id === product.id);
-        if (existingProduct) {
-          return prevCart.map((item) =>
-            item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
-          );
-        } else {
-          // return [...prevCart, { ...product, quantity: 1 }];
-          return [...prevCart, {
-            id: product.id,
-            name: product.name,
-            price: product.price,
-            quantity: 1,
-          }];
-        }
-      });
+        setCart((prevCart) => {
+            const existingProduct = prevCart.find(
+                (item) => item.id === product.id
+            );
+            if (existingProduct) {
+                return prevCart.map((item) =>
+                    item.id === product.id
+                        ? { ...item, quantity: item.quantity + 1 }
+                        : item
+                );
+            } else {
+                // return [...prevCart, { ...product, quantity: 1 }];
+                return [
+                    ...prevCart,
+                    {
+                        id: product.id,
+                        name: product.name,
+                        price: product.price,
+                        quantity: 1,
+                    },
+                ];
+            }
+        });
     };
 
     const removeProductFromCart = (productId) => {
-      setCart((prevCart) => {
-        return prevCart.reduce((acc, item) => {
-          if (item.id === productId) {
-            if (item.quantity > 1) {
-              acc.push({ ...item, quantity: item.quantity - 1 });
-            }
-          } else {
-            acc.push(item);
-          }
-          return acc;
-        }, []);
-      });
+        setCart((prevCart) => {
+            return prevCart.reduce((acc, item) => {
+                if (item.id === productId) {
+                    if (item.quantity > 1) {
+                        acc.push({ ...item, quantity: item.quantity - 1 });
+                    }
+                } else {
+                    acc.push(item);
+                }
+                return acc;
+            }, []);
+        });
     };
 
     const clearCart = () => {
-      setCart([]);
+        setCart([]);
     };
 
     const addProductOnCart = (productId) => {
-      setCart((prevCart) => {
-        return prevCart.map((item) =>
-          item.id === productId ? { ...item, quantity: item.quantity + 1 } : item
-        );
-      });
+        setCart((prevCart) => {
+            return prevCart.map((item) =>
+                item.id === productId
+                    ? { ...item, quantity: item.quantity + 1 }
+                    : item
+            );
+        });
     };
 
     const deleteProduct = async (product) => {
-      try {
-        const response = await axios.delete(`/products/${product.id}`);
-        if (!response.status) {
-          return response.message;
+        try {
+            const response = await axios.delete(`/products/${product.id}`);
+            if (!response.status) {
+                return response.message;
+            }
+            setProducts((prevProducts) =>
+                prevProducts.filter((i) => i.id !== product.id)
+            );
+            return response;
+        } catch (error) {
+            console.error(error);
+            throw new Error(error);
         }
-        setProducts(prevProducts => prevProducts.filter((i) => i.id !== product.id));
-        return response;
-      } catch (error) {
-        console.error(error);
-        throw new Error(error);
-      }
-    }
+    };
 
     const deleteCategory = async (category) => {
-      try {
-        const response = await axios.delete(`/categories/${category.id}`);
-        if (!response.status) {
-          return response.message;
+        try {
+            const response = await axios.delete(`/categories/${category.id}`);
+            if (!response.status) {
+                return response.message;
+            }
+            setCategories((prevCategories) =>
+                prevCategories.filter((i) => i.id !== category.id)
+            );
+            setProducts((prevProducts) =>
+                prevProducts.filter((i) => i.category_id !== category.id)
+            );
+            return response;
+        } catch (error) {
+            console.error(error);
+            throw new Error(error);
         }
-        setCategories(prevCategories => prevCategories.filter(i => i.id !== category.id));
-        setProducts(prevProducts => prevProducts.filter(i => i.category_id !== category.id));
-        return response;
-      } catch (error) {
-        console.error(error);
-        throw new Error(error);
-      }
-    }
+    };
 
     return (
-      <ProductContext.Provider value={{ cart, setCart, addProductToCart, clearCart, products, categories, addProductOnCart, removeProductFromCart, deleteProduct, deleteCategory }}>
-        {children}
-      </ProductContext.Provider>
+        <ProductContext.Provider
+            value={{
+                cart,
+                setCart,
+                addProductToCart,
+                clearCart,
+                products,
+                categories,
+                addProductOnCart,
+                removeProductFromCart,
+                deleteProduct,
+                deleteCategory,
+            }}
+        >
+            {children}
+        </ProductContext.Provider>
     );
 };
