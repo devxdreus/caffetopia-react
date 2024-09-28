666b7b85d063ec81e533e517828fd54ab5d17beb
devxdreus <andricrafter007@gmail.com>
2024-09-28 16:23:39 +0800

    update domain to ip

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const BASE_URL =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000/api"
-    : "https://coffetopia.xyz/api";
+    : "http://8.215.11.16/api";
 
 export default axios.create({
   baseURL: BASE_URL,
